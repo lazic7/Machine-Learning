b377414918c063e5f5b7ffd004b89a974deb76b6
Lazic <lazicc9@gmail.com>
2022-04-19 22:06:45 +0200

    pola zad1.py

--- LV3/zad1.py
+++ LV3/zad1.py
@@ -1,4 +1,15 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
+mtcars = pd.read_csv('C:/Users/lazic/Documents/FERIT/PSU_LV/LV3/mtcars.csv')
+
+mtcars = mtcars.sort_values(by=['mpg'], ascending=False)
+print(mtcars.head(5).car)
+
+print("")
+
+mtcars = mtcars.sort_values(by=['mpg'], ascending=True)
+print(mtcars[(mtcars.cyl == 8)].head(3).car)
+
+print(mtcars[(mtcars.cyl == 6)].mpg.sum()/mtcars.car.count())
