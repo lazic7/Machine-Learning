907c7f1a0147f9e0c16d28a5c8e63259a382887d
Lazic <lazicc9@gmail.com>
2022-04-19 22:16:08 +0200

    zad1.py 4

--- LV3/zad1.py
+++ LV3/zad1.py
@@ -3,13 +3,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 mtcars = pd.read_csv('C:/Users/lazic/Documents/FERIT/PSU_LV/LV3/mtcars.csv')
+print(mtcars)
 
+print("")
 mtcars = mtcars.sort_values(by=['mpg'], ascending=False)
 print(mtcars.head(5).car)
 
 print("")
-
 mtcars = mtcars.sort_values(by=['mpg'], ascending=True)
 print(mtcars[(mtcars.cyl == 8)].head(3).car)
 
-print(mtcars[(mtcars.cyl == 6)].mpg.sum()/mtcars.car.count())
+print("")
+print("AVG potrosnja 6cyl: ",mtcars[(mtcars.cyl == 6)].mpg.sum() / mtcars[(mtcars.cyl == 6)].mpg.count())
+
+print("")
+print("AVG potrosnja 4cyl masa izmedju 2000 i 2200lbs: ",mtcars[(mtcars.cyl == 4) & (mtcars.wt >= 2.0) & (mtcars.wt <= 2.2)].mpg.sum()/mtcars[(mtcars.cyl == 4) & (mtcars.wt >= 2.0) & (mtcars.wt <= 2.2)].mpg.count())
