b3595b0fdbc56ed2ff7e3b4ae30bed95df7641b1
Lazic <lazicc9@gmail.com>
2022-04-21 19:16:47 +0200

    renamed LVr

--- LV3/zad1.py
+++ LV3/zad1.py
@@ -2,16 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-mtcars = pd.read_csv('C:/Users/lazic/Documents/FERIT/PSU_LV/LV3/mtcars.csv')
+mtcars = pd.read_csv('mtcars.csv')
 print(mtcars)
 
 print("")
-mtcars = mtcars.sort_values(by=['mpg'], ascending=False)
-print(mtcars.head(5).car)
+print(mtcars.sort_values(by=['mpg'], ascending=True).head(5)[['car','mpg']])
 
 print("")
-mtcars = mtcars.sort_values(by=['mpg'], ascending=True)
-print(mtcars[(mtcars.cyl == 8)].head(3).car)
+print(mtcars[(mtcars.cyl == 8)].sort_values(by=['mpg'], ascending=False).head(3)[['car','mpg']])
 
 print("")
 print("AVG potrosnja 6cyl: ",mtcars[(mtcars.cyl == 6)].mpg.sum() / mtcars[(mtcars.cyl == 6)].mpg.count())
@@ -20,13 +18,13 @@ print("")
 print("AVG potrosnja 4cyl masa izmedju 2000 i 2200lbs: ",mtcars[(mtcars.cyl == 4) & (mtcars.wt >= 2.0) & (mtcars.wt <= 2.2)].mpg.sum()/mtcars[(mtcars.cyl == 4) & (mtcars.wt >= 2.0) & (mtcars.wt <= 2.2)].mpg.count())
 
 print("")
-print("Broj automatic vozila: ", mtcars[(mtcars.am == 1)].car.count())
+print("Broj automatic vozila: ", mtcars[(mtcars.am == 0)].car.count())
 
 print("")
-print("Broj manual vozila: ", mtcars[(mtcars.am == 0)].car.count())
+print("Broj manual vozila: ", mtcars[(mtcars.am == 1)].car.count())
 
 print("")
 print("Broj automatic vozila hp>100: ", mtcars[(mtcars.am == 1) & (mtcars.hp > 100)].car.count())
 
-mtcars['heavy'] = mtcars.wt * 1000 * 0.454
-print(mtcars[['car','heavy']])
+mtcars['wt_in_kg'] = mtcars.wt * 1000 * 0.454
+print(mtcars[['car','wt_in_kg']])
