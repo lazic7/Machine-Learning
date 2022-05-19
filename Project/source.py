b9ba83e1dc3651c2606b425db908ce4bd68abce2
Lazic <lazicc9@gmail.com>
2022-05-19 15:04:49 +0200

    v1

--- Project/source.py
+++ Project/source.py
@@ -1,4 +1,14 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 
-df = pd.read_csv('Project/diabetes.csv')
+df = pd.read_csv('diabetes.csv')
 print(df.info())
+print(df.describe())
+print(df.isnull().sum())
+
+#provjeravamo korelaciju izmedju varijabli podataka
+sns.heatmap(df.corr(), annot=True, fmt =".2f", cmap="coolwarm")
+plt.show()
+
+
